3a87127a7e8242515174da8c70b284010b76d665
erickhanon <erick.hanon@gmail.com>
2023-10-10 14:06:57 -0300

    First commit

---
+++ src/main/java/br/com/erickhanon/todolist/user/UserModel.java
@@ -0,0 +1,13 @@
+package br.com.erickhanon.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+                
+                private String name;
+                private String email;
+                private String password;
+
+    
+}
